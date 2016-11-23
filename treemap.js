da74608e18deaaabe9472fa46501bcc10f8c2493
sisi2016 <sisi@mosaixsoft.com>
2016-11-23 15:56:25 -0800

    updated treemap.js

--- treemap.js
+++ treemap.js
@@ -1,161 +1,192 @@
 /*global $*/
 $(function(){
+
+  // 1. load header.html
+  $("#header").load("../../header.html",function(){
+    document.getElementById("brand").innerHTML="Heatmap";
+    document.getElementById("userinput").value="";
+  });
+
   var counter = 0;
   window.uniqueId = function(){
       return 'id-' + counter++
   }
 
-  var margin = {top: 30, right: 0, bottom: 20, left: 0},
-      width = 960,
-      height = 500 - margin.top - margin.bottom,
+  // global variables
+  var main = $("#main")
+  var margin = {top: 30, right: 0, bottom: 20, left: 0}
+  var showCpu = false,
+      showDisk = false,
+      showMem = false
+
+  var cpuData = {},
+      diskData = {},
+      memData = {},
+      allcpu = {},
+      alldisk = {},
+      allmem = {}
+
+  var fetchCpuData = function(){
+    // return $.getJSON("../api/heatmap/cpu").then(function(data){
+    return $.getJSON("../autoscaler/cpu2.json").then(function(data){
+        cpuData = data
+        if(data["cpuTotal"] > 0) showCpu = true
+        allcpu = getVMContainers(data, "allcpu")
+    })
+  }
+
+  var fetchDiskData = function(){
+    return $.getJSON("../api/heatmap/disk").then(function(data){
+        diskData = data
+        if(data["diskTotal"] > 0) showDisk = true
+        alldisk = getVMContainers(data, "alldisk")
+    })
+  }
+
+  var fetchMemData = function(){
+    return $.getJSON("../api/heatmap/mem").then(function(data){
+        memData = data
+        if(data["memTotal"] > 0) showMem = true
+        allmem = getVMContainers(data, "allmem")
+    })
+  }
+
+  var fetchAllData = function() {
+      return fetchCpuData()
+              // .then(fetchDiskData)
+              // .then(fetchMemData)
+  }
+
+
+  $.when(fetchAllData())
+   .then(function(){
+      console.log("cpu")
+      console.log(cpuData)
+      console.log("allcpu")
+      console.log(allcpu)
+      createResponsibleLegend('legend')
+
+      if(showCpu) createTreemap("cpu", cpuData)
+      if(showDisk) createTreemap("disk", diskData)
+      if(showMem) createTreemap("mem", memData)
+
+      // if(showCpu) createSimpleTreemap("allcpu", allcpu)
+  });
+
+  function createResponsibleLegend(eleId){
+    var width = $("#" + eleId).innerWidth(),
+        formatNumber = d3.format(",%"),
+        colorDomain = [0, 0.5, 1.0],
+        // colorRange = ["#373a93", 'white', "#936638"],
+        colorRange = ["#33A133", "#385238", "#873535"],
+        sectionWidth = width/5
+
+        // adding a color scale
+    var color = d3.scale.linear()
+        .domain(colorDomain)
+        .range(colorRange)
+
+    var legend = d3.select("#"+eleId).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      // .attr("width", width)
+      .attr("height", 30)
+      .attr('class', 'legend')
+      .selectAll("g")
+          // .data([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
+          .data([0,1,2,3,4])
+          .enter()
+          .append('g')
+    
+    var legends = legend.append("g")
+                  .attr("transform", function(d) { return "translate(1,1)"; })
+                  .attr("class", "highlight")
+                  .on("click", function(d){
+                    console.log(d)
+                    // TO-DO: Add event listener to filter the treemap
+                  })
+        
+    legends.append("rect")
+        .attr("x", function(d){return margin.left + d * sectionWidth})
+        // .attr("x", function(d){return d * sectionWidth })
+        .attr("y", 0)
+        .attr("fill", function(d) {return color(colorIncrements(colorDomain, d))})
+        .attr('width', sectionWidth +"px")
+        .attr('height', '30px')
+
+    legends.append("text")
+            .text(function(d){return formatNumber(colorIncrements(colorDomain, d))})
+            .attr('y', 20)
+            // .attr('x', function(d){return margin.left + d * 80 + 40})
+            .attr('x', function(d){return d * sectionWidth + sectionWidth/2})
+  }
+
+  function createTreemap(mapName, root){
+    var map = main.append("<div class='row treemap'></div>")
+    map.append("<h3>"+ mapName + "</h3>")
+    map.append("<div id='" + mapName + "'></div>")
+    var width = $("#"+mapName).innerWidth(),
+      height = 500,
       formatNumber = d3.format(",%"),
       colorDomain = [0, 0.5, 1.0],
       // colorRange = ["#373a93", 'white', "#936638"],
-      colorRange = ["#33A133", "#385238", "#873535"],
+      colorRange = ["#33A133", "#742D2D", "#D62222"],
       transitioning;
   
-  // sets x and y scale to determine size of visible boxes
-  var x = d3.scale.linear()
-      .domain([0, width])
-      .range([0, width]);
-  
-  var y = d3.scale.linear()
-      .domain([0, height])
-      .range([0, height]);
-  
-  // adding a color scale
-  var color = d3.scale.linear()
-      .domain(colorDomain)
-      .range(colorRange);
-  
-  // introduce color scale here
-  
-  var treemap = d3.layout.treemap()
-      .children(function(d, depth) { return depth ? null : d._children; })
-      .sort(function(a, b) { return a.value - b.value; })
-      .ratio(height / width * 0.5 * (1 + Math.sqrt(5)))
-      .round(false);
-  
-  var svg = d3.select("#chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.bottom + margin.top)
-      .style("margin-left", -margin.left + "px")
-      .style("margin.right", -margin.right + "px")
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .style("shape-rendering", "crispEdges")
-      .attr("id", uniqueId)
-  
-  var grandparent = svg.append("g")
-      .attr("class", "grandparent")
-      .attr("id", uniqueId)
-  
-  grandparent.append("rect")
-      .attr("y", -margin.top)
-      .attr("width", width)
-      .attr("height", margin.top)
-      .attr("id", uniqueId)
-  
-  grandparent.append("text")
-      .attr("x", 6)
-      .attr("y", 6 - margin.top)
-      .attr("dy", ".75em")
-      .attr("id", uniqueId)
-  
-  var legend = d3.select("#legend").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", 30)
-    .attr('class', 'legend')
-    .selectAll("g")
-        // .data([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
-        .data([0,1,2,3,4])
-        .enter()
-        .append('g')
-  
-  // functions
-  
-  function initialize(root) {
-      root.x = root.y = 0;
-      root.dx = width;
-      root.dy = height;
-      root.depth = 0;
-    }
-  
-    // Aggregate the values for internal nodes. This is normally done by the
-    // treemap layout, but not here because of our custom implementation.
-    // We also take a snapshot of the original children (_children) to avoid
-    // the children being overwritten when when layout is computed.
-    function accumulate(d) {
-      return (d._children = d.children)
-        // recursion step, note that p and v are defined by reduce
-          ? d.value = d.children.reduce(function(p, v) {return p + accumulate(v); }, 0)
-          : d.value;
-    }
-  
-  
-  
-    // Compute the treemap layout recursively such that each group of siblings
-    // uses the same size (1×1) rather than the dimensions of the parent cell.
-    // This optimizes the layout for the current zoom state. Note that a wrapper
-    // object is created for the parent node for each group of siblings so that
-    // the parent’s dimensions are not discarded as we recurse. Since each group
-    // of sibling was laid out in 1×1, we must rescale to fit using absolute
-    // coordinates. This lets us use a viewport to zoom.
-    function layout(d) {
-      if (d._children) {
-        // treemap nodes comes from the treemap set of functions as part of d3
-        treemap.nodes({_children: d._children});
-        d._children.forEach(function(c) {
-          c.x = d.x + c.x * d.dx;
-          c.y = d.y + c.y * d.dy;
-          c.dx *= d.dx;
-          c.dy *= d.dy;
-          c.parent = d;
-          // recursion
-          layout(c);
-        });
-      }
-    }
-  
-  function colorIncrements(d){
-      return (colorDomain[colorDomain.length - 1] - colorDomain[0])/4*d + colorDomain[0];
-  }
-  
-  
-  var legends = legend.append("g")
-                .attr("transform", function(d) { return "translate(1,1)"; })
-                .attr("class", "highlight")
-                .on("click", function(d){
-                  console.log(d)
-                  // TO-DO: Add event listener to filter the treemap
-                })
-      
-  legends.append("rect")
-      .attr("x", function(d){return margin.left + d * 80})
-      .attr("y", 0)
-      .attr("fill", function(d) {return color(colorIncrements(d))})
-      .attr('width', '80px')
-      .attr('height', '30px')
+    console.log("Treemap:")
+    console.log("width: " + width + "; height: " + height)
 
-  legends.append("text")
-          .text(function(d){return formatNumber(colorIncrements(d))})
-          .attr('y', 20)
-          .attr('x', function(d){return margin.left + d * 80 + 40})
+    // sets x and y scale to determine size of visible boxes
+    var x = d3.scale.linear()
+        .domain([0, width])
+        .range([0, width]);
+    
+    var y = d3.scale.linear()
+        .domain([0, height])
+        .range([0, height]);
+    
+    // adding a color scale
+    var color = d3.scale.linear()
+        .domain(colorDomain)
+        .range(colorRange);
+    
+    // introduce color scale here
+    
+    var treemap = d3.layout.treemap()
+        .children(function(d, depth) { return depth ? null : d._children; })
+        .sort(function(a, b) { return a.value - b.value; })
+        .ratio(height / width * 0.5 * (1 + Math.sqrt(5)))
+        .round(false);
+    
+    var svg = d3.select("#"+mapName).append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("margin-left", -margin.left + "px")
+        .style("margin.right", -margin.right + "px")
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .style("shape-rendering", "crispEdges")
+        .attr("id", uniqueId)
+    
+    var grandparent = svg.append("g")
+        .attr("class", "grandparent")
+        .attr("id", uniqueId)
+    
+    grandparent.append("rect")
+        .attr("y", -margin.top)
+        .attr("width", width)
+        .attr("height", margin.top)
+        .attr("id", uniqueId)
+    
+    grandparent.append("text")
+        .attr("x", 6)
+        .attr("y", 6 - margin.top)
+        .attr("dy", ".75em")
+        .attr("id", uniqueId)
 
-          
-  // determines if white or black will be better contrasting color
-  function getContrast50(hexcolor){
-      return (parseInt(hexcolor.replace('#', ''), 3) > 0xffffff/3) ? 'black':'white';
-  }
-  
-  d3.json("data/cpu.json", function(root) {
-    // console.log(root)
-
-    parseData(root)
-    // initialize(root);
-    // accumulate(root);
-    // layout(root);
-    // display(root);
+    initialize(root);
+    accumulate(root);
+    layout(root);
+    display(root);
   
     function display(d) {
       grandparent
@@ -169,7 +200,8 @@ $(function(){
       grandparent
         .datum(d.parent)
         .select("rect")
-        .attr("fill", function(){console.log(color(d.rate)); return color(d['rate'])})
+        // .attr("fill", function(){console.log(color(d.rate)); return color(d['colorRate'])})
+        .attr("fill", "#cccccc")
         .attr("id", uniqueId)
   
       var g1 = svg.insert("g", ".grandparent")
@@ -198,7 +230,7 @@ $(function(){
           .attr("id", uniqueId)
           .call(rect)
         .append("title")
-          .text(function(d) {console.log(typeof(d.value), d.value); return d.name + ', value: ' + d.value + ', percent : ' + formatNumber(d.rate); });
+          .text(function(d) {console.log(typeof(d.value), d.value); return d.name + ', value: ' + d.title + ', percent : ' + formatNumber(d.rate); });
   
       g.append("text")
           .attr("dy", ".75em")
@@ -207,7 +239,7 @@ $(function(){
           // .call(text);
           .attr("x", function(d) { return x(d.x) + 6; })
           .attr("y", function(d) { return y(d.y) + 6; })
-          .attr("fill", function (d) {return getContrast50(color(parseFloat(d.rate)))})
+          .attr("fill", function (d) {return getContrast50(color(parseFloat(d.colorRate)))})
           .style("opacity", function(d){
 
 
@@ -267,38 +299,216 @@ $(function(){
   
       return g;
     }
-  
+
+    // functions
+    function initialize(root) {
+      root.x = root.y = 0;
+      root.dx = width;
+      root.dy = height;
+      root.depth = 0;
+    }
+
+    // Compute the treemap layout recursively such that each group of siblings
+    // uses the same size (1×1) rather than the dimensions of the parent cell.
+    // This optimizes the layout for the current zoom state. Note that a wrapper
+    // object is created for the parent node for each group of siblings so that
+    // the parent’s dimensions are not discarded as we recurse. Since each group
+    // of sibling was laid out in 1×1, we must rescale to fit using absolute
+    // coordinates. This lets us use a viewport to zoom.
+    function layout(d) {
+      if (d._children) {
+        // treemap nodes comes from the treemap set of functions as part of d3
+        treemap.nodes({_children: d._children});
+        d._children.forEach(function(c) {
+          c.x = d.x + c.x * d.dx;
+          c.y = d.y + c.y * d.dy;
+          c.dx *= d.dx;
+          c.dy *= d.dy;
+          c.parent = d;
+          // recursion
+          layout(c);
+        });
+      }
+    }
+
     function text(text) {
       text.attr("x", function(d) { return x(d.x) + 6; })
           .attr("y", function(d) { return y(d.y) + 6; })
-          .attr("fill", function (d) {return getContrast50(color(parseFloat(d.rate)))})
+          .attr("fill", function (d) {return getContrast50(color(parseFloat(d.colorRate)))})
     }
-  
+
     function rect(rect) {
       rect.attr("x", function(d) { return x(d.x); })
           .attr("y", function(d) { return y(d.y); })
           .attr("width", function(d) { return x(d.x + d.dx) - x(d.x); })
           .attr("height", function(d) { return y(d.y + d.dy) - y(d.y); })
-          .attr("fill", function(d){return color(parseFloat(d.rate));});
+          .attr("fill", function(d){return color(parseFloat(d.colorRate));});
     }
-  
-    function name(d) {
-      return d.parent
-          ? name(d.parent) + " / " + d.name
-          : d.name;
-    }
-  
-  });
-  
-  function parseData(json){
-    console.log("Before: ")
-    console.dir(json)
-
-    var data = {}
-    // TO-DO: parse data
-    
-    // console.log("After: ")
-    // console.dir(data)
-    return data
   }
+
+  function createSimpleTreemap(mapName, data){
+    var simpleMap = main.append("<div class='row simpleTreemap'></div>")
+    simpleMap.append("<h3>"+ mapName + "</h3>")
+    simpleMap.append("<div id='" + mapName + "'></div>")
+
+    var width = $("#"+mapName).innerWidth(),
+        height = 500,
+        formatNumber = d3.format(",%"),
+        colorDomain = [0, 0.5, 1.0],
+        colorRange = ["#33A133", "#742D2D", "#D62222"]
+
+    // sets x and y scale to determine size of visible boxes
+    var x = d3.scale.linear()
+        .domain([0, width])
+        .range([0, width])
+    
+    var y = d3.scale.linear()
+        .domain([0, height])
+        .range([0, height])
+    
+    // adding a color scale
+    var color = d3.scale.linear()
+        .domain(colorDomain)
+        .range(colorRange)
+
+    var treemap = d3.layout.treemap()
+                  .size([width, height])
+                  .sticky(true)
+                  .value(function(d) { return d.rate; });
+ 
+    var node = d3.select("#"+mapName).append("svg")
+              .attr("width", width)
+              .attr("height", height)
+              .style("margin-left", -margin.left + "px")
+              .style("margin.right", -margin.right + "px")
+              .datum(data)
+              .selectAll(".node")
+                .data(treemap.nodes)
+              .enter().append("div")
+                .attr("class", "node")
+                .call(position)
+                .style("background-color", function(d) {
+                    return d.type == 'root' ? '#fff' : color(parseFloat(d.colorRate)); })
+                .append('div')
+                .style("font-size", function(d) {
+                    // compute font size based on sqrt(area)
+                    // return Math.max(20, 0.18*Math.sqrt(d.rate))+'px'; })
+                    return '20px'; })
+                .text(function(d) { return d.children ? null : d.name; });
+
+      function position() {
+        this.style("left", function(d) { return d.x + "px"; })
+            .style("top", function(d) { return d.y + "px"; })
+            .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+            .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+      }
+  }
+
+  function name(d) {
+    return d.parent
+        ? name(d.parent) + " / " + d.name
+        : d.name;
+  }
+  
+  // Aggregate the values for internal nodes. This is normally done by the
+  // treemap layout, but not here because of our custom implementation.
+  // We also take a snapshot of the original children (_children) to avoid
+  // the children being overwritten when when layout is computed.
+  function accumulate(d) {
+    return (d._children = d.children)
+      // recursion step, note that p and v are defined by reduce
+        ? d.value = d.children.reduce(function(p, v) {return p + accumulate(v); }, 0)
+        : d.value;
+  }
+
+
+  // determines if white or black will be better contrasting color
+  function getContrast50(hexcolor){
+      return (parseInt(hexcolor.replace('#', ''), 3) > 0xffffff/3) ? 'black':'white';
+  }
+
+  function colorIncrements(colorDomain, d){
+    return (colorDomain[colorDomain.length - 1] - colorDomain[0])/4*d + colorDomain[0];
+  }
+
+  function parseData(json){
+    var data = {}
+
+    return data;
+  }
+
+  /**
+   * [genChildrenRate description]
+   * @param  {[type]} children [description]
+   * @return {[type]}          [description]
+   */
+  function genChildrenRate(children){
+    var myChildren =  children.slice(0)
+    var colorRange = [0.1, 1.0]
+
+    var len = myChildren.length,
+        array = [],
+        total = 0
+
+    if(len > 0){
+      for(var i=0; i<len; i++){
+        var child = myChildren[i]
+        array.push(child)
+        total += child.title
+      }
+
+      for(var i=0; i<array.length; i++){
+        var tmp = array[i]
+        var colorRate = colorRange[0]
+        tmp.rate = tmp.title / total
+
+        if(tmp.value === 0){
+          tmp.colorRate = colorRate
+        }else{
+          colorRate = tmp.used/tmp.value
+          if(colorRate>colorRange[1]) colorRate = colorRange[1]
+          if(colorRate<colorRange[0]) colorRate = colorRange[0]
+          tmp.colorRate = colorRate
+        }
+      }
+    }
+    return array
+  }
+
+  function getVMContainers(json, type){
+    var VMContainers = {
+      "name": type,
+      "type": "root",
+      "children":[]
+    }
+
+    var children = []
+    var data = JSON.parse(JSON.stringify(json))
+
+    if(data.children){
+      
+    }
+    recurse(json)
+
+    function recurse(node) {
+        if(node.children !== undefined){
+          node.children.forEach(function(node){
+            recurse(node);
+          })
+        }else{
+          if(node.type == "VM" || node.type == "CONTAINER"){
+            var tmp = JSON.parse(JSON.stringify(node))
+            children.push(tmp)
+          }
+        }
+    }
+
+    VMContainers.children =  genChildrenRate(children)
+
+
+    return VMContainers
+
+  }
+
 })
+
